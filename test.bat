d580065e2b130a68037dc2a9cbb2e93a603c2e06
B vil <villeriusborro@gmail.com>
2021-10-13 21:43:36 +0200

    test build 2021-10-13 21:42

--- test.bat
+++ test.bat
@@ -1,8 +1,9 @@
 @echo off
+set /p psw=D:\pypi_psw\test_pypi.txt
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%
 py -m build
-py -m twine upload --repository testpypi dist/*
+py -m twine upload -u btpv -p %psw% --repository testpypi dist/*
 rmdir dist /s
 git add *
 git commit -m "test build %ldt%"
