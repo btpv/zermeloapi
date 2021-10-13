05146b84f2174b4c390832c9b437752c3930556e
B vil <villeriusborro@gmail.com>
2021-10-13 21:53:58 +0200

    build 2021-10-13 21:53

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 @echo off
-for /f "delims== tokens=1,2" %%G in (D:\pypi_psw\test_pypi.txt) do set %%G=%%H
+for /f "delims== tokens=1,2" %%G in (D:\pypi_psw\pypi.txt) do set %%G=%%H
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%
 py -m build
