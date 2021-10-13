73616b64123d8e5b773cad878a0b9dcbbd65569b
B vil <villeriusborro@gmail.com>
2021-10-13 21:50:36 +0200

    build 2021-10-13 21:50

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 
 set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%
 py -m build
 py -m twine -u btpv -p %pypi% upload dist/*
-rmdir dist /s
+rmdir dist /S /Q
 git add *
 git commit -m "build %ldt%"
 git push -u origin main
