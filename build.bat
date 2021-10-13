f5bb673d00c27bd9779333152cb66d8742fd817a
B vil <villeriusborro@gmail.com>
2021-10-13 13:09:11 +0200

    build 2021-10-13 13:09

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
-set ldt=%ldt:~0,4%%ldt:~4,2%%ldt:~6,2%%ldt:~8,2%%ldt:~10,2%
+set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%
 @REM py -m build
 @REM py -m twine upload dist/*
 @REM rmdir dist /s
