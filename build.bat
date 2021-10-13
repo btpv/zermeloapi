e2f21b610fb8279ee88e96e0aed38285917f5e50
B vil <villeriusborro@gmail.com>
2021-10-13 21:49:58 +0200

    build 2021-10-13 21:49

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 @echo off
-set /p psw=D:\pypi_psw\test_pypi.txt 
+for /f "delims== tokens=1,2" %%G in (D:\pypi_psw\test_pypi.txt) do set %%G=%%H
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%
 py -m build
-py -m twine upload dist/* -u btpv -p %psw%
+py -m twine -u btpv -p %pypi% upload dist/*
 rmdir dist /s
 git add *
 git commit -m "build %ldt%"
--- test.bat
+++ test.bat
@@ -1,8 +1,8 @@
 @echo off
-for /f "delims== tokens=1,2" %%G in (param.txt) do set %%G=%%H
+for /f "delims== tokens=1,2" %%G in (D:\pypi_psw\test_pypi.txt) do set %%G=%%H
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%
-@REM py -m build
+py -m build
 py -m twine upload -u btpv -p %testpypi% --repository testpypi dist/*
 rmdir dist /s
 git add *
