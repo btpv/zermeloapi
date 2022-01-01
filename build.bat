ab083f77e80a408923ea3929a0215b0ad52845cc
btpv <villeriusborro@gmail.com>
2022-01-01 23:23:03 +0100

    build 2022-01-01 23:20

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@
 for /f "delims== tokens=1,2" %%G in (D:\pypi_psw\pypi.txt) do set %%G=%%H
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%
-py -m build
-py -m twine upload -u btpv -p %pypi%  dist/*
+python -m build
+python -m twine upload -u btpv -p %pypi%  dist/*
 rmdir dist /S /Q
 rmdir "src/zermeloapi.egg-info" /S /Q
 git add *
