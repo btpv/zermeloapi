5105f154d840ad12783e39236f2012e4213976fb
btpv <villeriusborro@gmail.com>
2022-08-26 11:00:33 +0200

    build 2022-08-26 11:00:33.187185

--- build.py
+++ build.py
@@ -3,6 +3,6 @@ with open('../../pws.yaml') as file:
     documents = yaml.full_load(file)
     pws = documents["twine"]
 vers = f"build {datetime.datetime.now()}"
-print(os.system(f'pythons -m build && pythons -m twine upload -u btpv -p {pws} dist/*'))
+print(os.system(f'python3 -m build && python3 -m twine upload -u btpv -p {pws} dist/*'))
 print(os.system(f'git add * && git commit -m "{vers}" && git push -u origin main && pip install --upgrade zermeloapi'))
 print(vers)
