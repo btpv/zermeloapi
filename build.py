6d9813b814694d2f77df012715931337b36caa25
btpv <villeriusborro@gmail.com>
2022-08-26 10:49:27 +0200

    build 2022-08-26 10:49:27.346959

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
-from datetime import datetime
-import yaml,datetime
+import yaml,datetime,os
 with open('../../pws.yaml') as file:
     documents = yaml.full_load(file)
-    print(documents["twine"])
+    pws = documents["twine"]
 vers = f"build {datetime.datetime.now()}"
+os.system(f'python -m build && python -m twine upload -u btpv -p {pws} dist/*')
+os.system(f'git add * && git commit -m "{vers}" && git push -u origin main && pip install --upgrade zermeloapi')
