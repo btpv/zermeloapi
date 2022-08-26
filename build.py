4bb2579fbf18a07e2f96f2872aab2832ae6abf8e
btpv <villeriusborro@gmail.com>
2022-08-26 10:51:41 +0200

    build 2022-08-26 10:51:41.902220

--- build.py
+++ build.py
@@ -5,3 +5,4 @@ with open('../../pws.yaml') as file:
 vers = f"build {datetime.datetime.now()}"
 print(os.system(f'pythons -m build && pythons -m twine upload -u btpv -p {pws} dist/*'))
 print(os.system(f'git add * && git commit -m "{vers}" && git push -u origin main && pip install --upgrade zermeloapi'))
+print(vers)
