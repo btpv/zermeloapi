2ddc29cbdad2c5e71636990ae7f0e73e216e5455
btpv <62651117+btpv@users.noreply.github.com>
2022-08-30 13:24:32 +0200

    Delete buildzmlapi.py

--- buildzmlapi.py
+++
@@ -1,8 +0,0 @@
-import yaml,datetime,os
-with open(f'/pws.yaml') as file:
-    documents = yaml.full_load(file)
-    pws = documents["twine"]
-vers = f"build {datetime.datetime.now()}"
-# print(os.system(f'python3 -m build && python3 -m twine upload -u btpv -p {pws} dist/*'))
-# print(os.system(f'git add * && git commit -m "{vers}" && git push -u origin main && pip install --upgrade zermeloapi'))
-print(vers)
