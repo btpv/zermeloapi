998e3e23fc76ecdf5d4a21fb36900379a9ca2d5e
btpv <villeriusborro@gmail.com>
2022-08-31 11:39:43 +0200

    build 2022-08-31 11:39:19.329571

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
+cp "./src/zermeloapi/__init__.py" "./src\zermeloapi\__init__.py"
 VERS=`python3 -c 'exec("import datetime\nprint(datetime.datetime.now())")'`
 VERS="build $VERS"
 echo "$VERS"
 PWS=`cat ~/pws/twine`
+ls src/zermeloapi
 python3 -m build
-# python3 -m twine upload -u btpv -p $PWS dist/*
-# git add * 
-# git commit -m "$VERS" 
-# git push -u origin main
-# pip install --upgrade zermeloapi
+python3 -m twine upload -u btpv -p $PWS dist/*
+rm dist -r
+rm src\zermeloapi\__init__.py"
+git add * 
+git commit -m "$VERS" 
+git push -u origin main
+pip install --upgrade zermeloapi
