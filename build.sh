bb9f4859fec03e6ba1b83f5c8530afff7f35d73b
btpv <villeriusborro@gmail.com>
2022-08-31 11:48:51 +0200

    build 2022-08-31 11:48:33.500727

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ VERS="build $VERS"
 echo "$VERS"
 PWS=`cat ~/pws/twine`
 python3 -m build
-python3 -m twine upload -u btpv -p $PWS dist/*
+# python3 -m twine upload -u btpv -p $PWS dist/*
 rm dist -r
 rm ./"src\zermeloapi\__init__.py"
-git add * 
-git commit -m "$VERS" 
-git push -u origin main
-pip install --upgrade zermeloapi
+# git add * 
+# git commit -m "$VERS" 
+# git push -u origin main
+# pip install --upgrade zermeloapi
