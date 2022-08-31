b6c06c985300024bb04419e5227d61bf9723d1df
btpv <villeriusborro@gmail.com>
2022-08-31 11:40:19 +0200

    build 2022-08-31 11:39:55.997736

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ ls src/zermeloapi
 python3 -m build
 python3 -m twine upload -u btpv -p $PWS dist/*
 rm dist -r
-rm src\zermeloapi\__init__.py"
+rm ./"src\zermeloapi\__init__.py"
 git add * 
 git commit -m "$VERS" 
 git push -u origin main
