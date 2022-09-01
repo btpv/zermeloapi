c906d8fcec7bed1f8edf43a47a1c8c215089d177
btpv <villeriusborro@gmail.com>
2022-09-01 11:37:59 +0200

    build 2022-09-01 11:37:34.027018

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cp "./src/zermeloapi/__init__.py" "src\zermeloapi\__init__.py"
-python3 -c "exec('path = \'src/zermeloapi.egg-info\'\nfrom os import listdir,system\nfrom os.path import join\nitems = []\nfor i in listdir(path):\n\titems.append([join(path,i),join(path,i).replace(\'/\',\'\\\\\\\\\\\\\\\\\')])\nprint(items)\nfor i in items:\n\tsystem(f\'cp {i[0]} {i[1]}\')')"
+python3 -c "exec('path = \'src/zermeloapi.egg-info\'\nfrom os import listdir,system\nfrom os.path import join\nitems = []\nfor i in listdir(path):\n\titems.append([join(path,i),join(path,i).replace(\'/\',\'\\\\\\\\\\\\\\\\\')])\nfor i in items:\n\tsystem(f\'cp {i[0]} {i[1]}\')')"
 VERS=`python3 -c 'exec("import datetime\nprint(datetime.datetime.now())")'`
 VERS="build $VERS"
 echo "$VERS"
