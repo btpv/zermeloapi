de423a52b773d72d7a318d2b14f14aa60897bd0a
btpv <villeriusborro@gmail.com>
2022-09-01 11:49:47 +0200

    test build 2022-09-01 11:49:23.566261

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ VERS="test build $VERS"
 echo "$VERS"
 PWS=`cat ~/pws/testtwine`
 python3 -m build -s
+python3 -c "exec('path = \'src/zermeloapi.egg-info\'\nfrom os import listdir,system\nfrom os.path import join\nitems = []\nfor i in listdir(path):\n\titems.append([join(path,i),join(path,i).replace(\'/\',\'\\\\\\\\\\\\\\\\\')])\nfor i in items:\n\tsystem(f\'cp {i[0]} {i[1]}\')')"
 python3 -m build -w
 python3 -m twine upload -u btpv --repository testpypi -p $PWS dist/*
 rm dist -r
@@ -14,5 +15,6 @@ rm src\\zermeloapi*
 git add * 
 git commit -m "$VERS" 
 git push -u origin main
+sleep 5
 pip install -i https://test.pypi.org/simple/ zermeloapi
 python3 test.py
