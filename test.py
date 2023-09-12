1e15df3159db68cf7fae53bc78b7c1b37dab213a
btpv <villeriusborro@gmail.com>
2023-09-12 08:49:37 +0200

    update

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-from os import device_encoding
-import zermeloapi
+import src.zermeloapi as zermeloapi
 zml = zermeloapi.zermelo(school="ozhw",username="212019",password="rooster1",debug=True)
 zml.print_schedule()
